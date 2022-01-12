b0c45b4dfac5aa5f2c7364f4edaaaf36ded16566
Johnny Wu <johnnyw131@gmail.com>
2022-01-11 19:51:47 -0500

    Completed

---
+++ lib/Ticket.rb
@@ -0,0 +1,15 @@
+class Ticket
+attr_accessor: :name, :price, :title
+@@all = []
+    def initialize (name,titles)
+        @name = names
+        @title = title
+        @price = 5.00
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+    
+end
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,10 +1,13 @@
 def initialize_app 
-    movies = ['2001: A Space Odyssey', 'The Godfather', 'Citizen Kane', 'Raiders of the Lost Ark', 'In the mood for love', 'Seven Samurai']
+    menu
 
-    puts "Welcome to Flatiron Movies"
+end 
+
+def menu 
+puts "Welcome to Flatiron Movies"
     puts "Please choose an option:"
     puts "1. List all Movies"
-    puts "2. List Tickte holder names"
+    puts "2. List Ticket holder names"
     puts "3. Add ticket"
     puts "0. to exit"
     
@@ -12,10 +15,11 @@ def initialize_app
 
     case user_input
     when "1"
+        movies = ['2001: A Space Odyssey', 'The Godfather', 'Citizen Kane', 'Raiders of the Lost Ark', 'In the mood for love', 'Seven Samurai']
         movies.each {|movie| puts movie }
         
     when "2"
-
+puts Ticket.all.map{|t| t.name}
     when "3"
         puts create_ticket
         
@@ -25,11 +29,18 @@ def initialize_app
         puts "sorry we don't have that option"
         
     end 
+end
 
-end 
+
 
 def create_ticket
-   
+    puts "name"
+    name = gets.strip
+    puts 'Title'
+    title = gets.strip
+
+   t1= Ticket.new(name, title)
+   menu
 end 
 
 
